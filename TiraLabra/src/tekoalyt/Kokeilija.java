209368cb347679bc82cc5d999d28380ebf428bc3
Lassi J Vapaakallio <lvapaaka@dhp-71.cs.helsinki.fi>
2013-01-29 16:11:21 +0200

    Uusien tekoälyjen pohjat

---
+++ TiraLabra/src/tekoalyt/Kokeilija.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tekoalyt;
+
+/**
+ *
+ * @author lvapaaka
+ */
+public class Kokeilija {
+    
+}
